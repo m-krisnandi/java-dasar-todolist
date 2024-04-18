9e7360adce839f4cd6f02cb3b6c666ddac0cecea
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2024-04-18 09:50:44 +0700

    feature and testing remove todolist

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
         // testShowTodoList();
-        testAddTodoList();
+        // testAddTodoList();
+        testRemoveTodoList();
     }
 
     /**
@@ -60,6 +61,39 @@ public class Main {
         for (int i = 0; i < 25; i++) {
             addTodoList("Contoh Todo ke." + i);
         }
+
+        showTodoList();
+    }
+
+    public static boolean removeTodoList(Integer number) {
+        if ((number - 1) >= model.length) {
+            return false;
+        } else if (model[number - 1] == null) {
+            return false;
+        } else {
+            for (int i = (number - 1); i < model.length; i++) {
+                if (i == (model.length - 1)) {
+                    model[i] = null;
+                } else {
+                    model[i] = model[i + 1];
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void testRemoveTodoList() {
+        addTodoList("Satu");
+        addTodoList("Dua");
+        addTodoList("Tiga");
+        addTodoList("Empat");
+        addTodoList("Lima");
+
+        var result = removeTodoList(20);
+        System.out.println(result);
+
+        result = removeTodoList(2);
+        System.out.println(result);
 
         showTodoList();
     }
